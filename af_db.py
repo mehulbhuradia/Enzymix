952eb7b651d2cc04231b8911e032282a08d4346d
mehulbhuradia <mehulbhuradia007@gmail.com>
2024-01-30 22:01:36 +0530

    minor

--- af_db.py
+++ af_db.py
@@ -27,4 +27,4 @@ class ProtienStructuresDataset(Dataset):
         for e in data['edges']:
           edges.append(torch.tensor(e, dtype=torch.int64))
         # return coords, one_hot, v, edges
-        return coords, one_hot, 0, edges, file_path
+        return coords, one_hot, 0, edges, file_path.split('/')[-1].split('.')[0].split('_')[0]
