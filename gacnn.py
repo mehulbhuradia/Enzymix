8aee8b3a19cd1b6918d7798d1e6dcc5986ebb7d1
mehulbhuradia <mehulbhuradia007@gmail.com>
2024-03-06 08:31:34 +0530

    linear

--- gacnn.py
+++ gacnn.py
@@ -12,14 +12,16 @@ class GACNN(nn.Module):
         super(GACNN, self).__init__()
         self.add_module("in_linear_layer", nn.Linear(self.in_node_nf, self.hidden_nf))
         for i in range(0, n_layers):
-            self.add_module("gcl_%d" % i, GATv2Conv(self.hidden_nf,self.hidden_nf-3))
+            self.add_module("gcl_%d" % i, nn.Linear(self.hidden_nf,self.hidden_nf))
+            self.add_module("relu_%d" % i, nn.ReLU())
         self.add_module("out_linear_layer", nn.Linear(self.hidden_nf, self.out_node_nf))
 
     def forward(self, h, t, edges):
         h = torch.cat((h, t), dim=1)
         h = self._modules["in_linear_layer"](h)
         for i in range(0, self.n_layers):
-            h = self._modules["gcl_%d" % i](h, edges)
-            h = torch.cat((h, t), dim=1)
+            h = self._modules["gcl_%d" % i](h)
+            h = self._modules["relu_%d" % i](h)
+            # h = torch.cat((h, t), dim=1)
         h = self._modules["out_linear_layer"](h)
         return h
