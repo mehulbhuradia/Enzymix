867e8a619ee70af4635f512ec348263c4605fc17
mehulbhuradia <mehulbhuradia007@gmail.com>
2024-03-31 22:10:48 +0530

    removed exracode

--- egnn_complex.py
+++ egnn_complex.py
@@ -189,24 +189,8 @@ class EGNN(nn.Module):
                 raise KeyboardInterrupt()
             
             h = self._modules["embedding_out_%d" % i](h)
-            
-        return h, x
 
-        for i in range(0, self.n_layers):
-            res_len = h.shape[0]/batch_size.item()
-            indeces=torch.arange(res_len).unsqueeze(-1)
-            indeces = indeces.to(h.device)
-            pos_e = self.pos_enc(indeces)
-            pos_e = pos_e.to(h.device)
-            pos_chunks = []
-            for _ in range(batch_size):
-                pos_chunks.append(pos_e)
-            pos_h = torch.cat(pos_chunks, dim=0)
-            h = torch.cat((h, pos_h), dim=1)
-            h = torch.cat((h, t), dim=1)
-            h = self._modules["embedding_in_%d" % i](h)
-            h,_ = self._modules["gcl_%d" % i](h, edges)
-            h = self._modules["embedding_out_%d" % i](h)
+        return h, x
 
 
 
